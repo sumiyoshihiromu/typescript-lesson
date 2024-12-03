58255afcdd2154d9ee48249e357691c436e23e0e
sumiyoshihiromu <sumihiro127@icloud.com>
2024-12-03 10:28:38 +0900

    private修飾子追加

--- Person.ts
+++ Person.ts
@@ -1,9 +1,15 @@
 class Person {
-  name: string;
+  private name: string;
+  private age: number;
 
-  constructor(name: string) {
+  constructor(name: string, age: number) {
     this.name = name;
-  } 
+    this.age = age;
+  }
+
+  greeting() {
+    console.log(`Hello! My name is ${this.name}. I am ${this.age} years old.`);
+  }
 }
 
-const hiromu = new Person('hiromu');
+const hiromu = new Person('hiromu', 25);
