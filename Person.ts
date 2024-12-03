86b603cc9bd25e867af493addbbe9d40c0e798c3
sumiyoshihiromu <sumihiro127@icloud.com>
2024-12-03 11:40:59 +0900

    クラス継承

--- Person.ts
+++ Person.ts
@@ -1,10 +1,30 @@
 class Person {
-  constructor(private name: string, private age: number) {
+  constructor(protected name: string, protected age: number) {
   }
 
   greeting() {
     console.log(`Hello! My name is ${this.name}. I am ${this.age} years old.`);
   }
+
+  incrementAge() {
+    this.age += 1;
+  }
 }
 
 const hiromu = new Person('hiromu', 25);
+
+class Teacher extends Person {
+  constructor(name: string, age: number, public _subject: string) {
+    super(name, age);
+  }
+
+  get subject() {
+    return 'Music';
+  }
+
+  greeting() {
+    console.log(`Hello! My name is ${this.name}. I am ${this.age} years old. I teach ${this.subject}.`);
+  }
+}
+
+const teacher = new Teacher('Sumiyoshi', 26, 'Math');
